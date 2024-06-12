b03e2aa7de38ba11d8d6c33fd1c5729b1fba50ed
Nabokov79 <nabokovsg@yandex.ru>
2024-06-12 15:11:48 +0300

    add new

---
+++ diagnosedNK/src/main/java/ru/nabokovsg/diagnosedNK/DiagnosedNKApplication.java
@@ -0,0 +1,12 @@
+package ru.nabokovsg.diagnosedNK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosedNKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagnosedNKApplication.class, args);
+    }
+}
